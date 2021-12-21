3920bb37cd4e349be3be1851c762b413df2373e2
KulkovIvan <81376031+KulkovIvan@users.noreply.github.com>
2021-12-21 19:41:43 +0300

    Add files via upload

---
+++ items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InstaparserItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    my_username = scrapy.Field()
+    my_user_id = scrapy.Field()
+    _id = scrapy.Field()
+    other_user_photo = scrapy.Field()
+    other_user_fullname = scrapy.Field()
+    other_username = scrapy.Field()
+    other_user_id = scrapy.Field()
+    partition = scrapy.Field()
